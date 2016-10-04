12de0440bcc4c58326f64c34f9ca30f7b70d6787
graholm <graholm@nuevaschool.org>
2016-10-04 08:40:12 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var input = $("#not-a-real-id")
 input.keydwn(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    storeValue("gracesVariable", valueeeee);
+    console.log
+    storeValue("gracesVariable", value);
   }
 })
 
